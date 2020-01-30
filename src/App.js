d9310ce11e359b8347a224be4fd21c646a7827e5
pmarik <pmarik@uwalumni.com>
2020-01-30 15:46:48 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import axios from 'axios';
+import CardList from './components/card-list/Card-List.component';
+import { SearchBox } from './components/search-box/Search-Box.component';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component { 
+  constructor(){
+    super();
+
+    this.state = {
+      currencies: [],
+      searchField: ''
+    }
+  }
+
+  componentDidMount() {
+    // fetch('https://api.exchangeratesapi.io/latest?base=USD')
+    //   .then(res => res.json()
+    //   .then(users => console.log(users)))
+
+    axios.get('https://api.exchangeratesapi.io/latest?base=USD').then(res => {
+      const ratesArray = [];
+      let idPlaceholder = 0;
+
+      for (let [key, value] of Object.entries(res.data.rates)) {
+
+        let currencyObj = {
+          "id": idPlaceholder,
+          "name": key,
+          "rate": value
+        }
+        ratesArray.push(currencyObj);
+        idPlaceholder++;
+      }
+
+      this.setState({ currencies: ratesArray});
+    })
+  }
+
+  handleChange = (e) =>{
+   this.setState({ searchField: e.target.value }, () =>{
+   })
+  }
+
+  render(){
+    const { currencies, searchField } = this.state;
+    const filteredCurrencies = currencies.filter(currency => {
+      return currency.name.toLowerCase().includes(searchField.toLowerCase())
+     } );  
+
+    return (
+      <div className="App">
+        <h1> Exchange Rates </h1>
+        <SearchBox 
+          placeholder='search currencies'
+          handleChange={this.handleChange}
+        />
+        <CardList currencies={filteredCurrencies} />
+      </div>
+    );
+  }
 }
 
 export default App;
