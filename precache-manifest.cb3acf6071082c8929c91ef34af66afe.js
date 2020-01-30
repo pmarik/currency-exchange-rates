e07ebb0304e49b3f653c4d2e4b67ff8ea476218f
pmarik <pmarik@uwalumni.com>
2020-01-30 17:46:31 -0600

    Updates

---
+++ precache-manifest.cb3acf6071082c8929c91ef34af66afe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe109c1c0c64d9f04e18775f300681d0",
+    "url": "/currency-exchange-rates/index.html"
+  },
+  {
+    "revision": "1224f6c01cbf915ffb3a",
+    "url": "/currency-exchange-rates/static/css/main.d0941e65.chunk.css"
+  },
+  {
+    "revision": "c4deb2976f0427137418",
+    "url": "/currency-exchange-rates/static/js/2.476d919e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/currency-exchange-rates/static/js/2.476d919e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1224f6c01cbf915ffb3a",
+    "url": "/currency-exchange-rates/static/js/main.e1ba4cc3.chunk.js"
+  },
+  {
+    "revision": "02311346ebd36dda5a22",
+    "url": "/currency-exchange-rates/static/js/runtime-main.94a3941e.js"
+  }
+]);
